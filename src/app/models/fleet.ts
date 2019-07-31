a5eab1bc45e6d9766590dc946bd1cf41df5a951c
Ian McCallum <ianfmc@gmail.com>
2019-07-30 18:49:36 -0700

    Remove redundant Fleet component CSS

--- src/app/models/fleet.ts
+++ src/app/models/fleet.ts
@@ -10,5 +10,6 @@ export const FLEET: Fleet[] =  [
     { id: '1', name: 'Intrepid', owner: 'Pete Smith', model: 'Catalina 320', type: 'Sail'},
     { id: '2', name: 'Razor', owner: 'Andy Jones', model: 'Hyland 52', type: 'Power'},
     { id: '3', name: 'Scooter', owner: 'Jane Dough', model: 'Lagoon 450', type: 'Sail'},
-    { id: '4', name: 'Shamrock', owner: 'Will Turner', model: 'Hatteras 38', type: 'Power'}
+    { id: '4', name: 'Shamrock', owner: 'Will Turner', model: 'Hatteras 38', type: 'Power'},
+    { id: '5', name: 'Courageous', owner: 'Ted Turner', model: 'Swan 56', type: 'Sail'}
 ];
